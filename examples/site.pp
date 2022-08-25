0a150ce8d43b5933251896a49cce55d681440f5f
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2022-08-25 17:21:02 +0200

    example complete

--- examples/site.pp
+++ examples/site.pp
@@ -1,11 +1,3 @@
 node default {
   include i2pd
-  include i2pd::daemon::ntcp2
-  include i2pd::daemon::ssu2
-  include i2pd::daemon::http
-  include i2pd::daemon::httpproxy
-  include i2pd::daemon::sam
-  include i2pd::daemon::socksproxy
-  include i2pd::daemon::bob
-  include i2pd::daemon::i2cp
 }
